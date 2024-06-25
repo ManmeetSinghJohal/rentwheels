d3fa2176f4c7c17e545753e223b551de752a0282
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-25 19:34:44 +0100

    add isChecked prop

--- components/Search/CheckBoxes.tsx
+++ components/Search/CheckBoxes.tsx
@@ -4,10 +4,10 @@ import { Checkbox } from "../ui/checkbox";
 import { Label } from "../ui/label";
 import React from "react";
 
-const CheckBoxes = ({ handleChange, category, type, capacity }: any) => {
+const CheckBoxes = ({ handleChange, category, type, capacity, isChecked }: any) => {
   return (
     <div className="flex gap-2 items-center mb-8">
-      <Checkbox id={type || capacity} value="" className="w-6 h-6 rounded-md" onClick={() => handleChange(category, type || capacity)} />
+      <Checkbox id={type || capacity} className="w-6 h-6 rounded-md" onClick={() => handleChange(category, type || capacity)} checked={isChecked}/>
       <Label htmlFor="Sport" className="text-gray-700 font-semibold text-xl leading-6 dark:text-white-100">
         {type || `${capacity} person`}
       </Label>
